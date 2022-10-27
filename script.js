ac4fd2bde226948f4544db262926be242ddabe50
art3008 <art.osv@mail.ru>
2022-10-27 10:40:06 +0600

    done

--- script.js
+++ script.js
@@ -4,39 +4,38 @@ const iconStar = document.querySelector('.icon_star')
 const title = document.querySelector('.title')
 const descrip = document.querySelector('.description')
 const thkImg = document.querySelector('.thankYouImg')
-
+const result = document.querySelector('.result_text')
+const bottom = document.querySelector('.bottom')
 let enterNum = ''
 
 
 btnRaiting.addEventListener('click', function(event) {
     enterNum = event.target.innerText
-
-    // descrip.innerHTML  = '<img src="./images/illustration-thank-you.svg">'
-    // descrip.style.visibility = 'hidden'
-    iconStar.style.display = 'none'
-    title.style.display = 'none'
-    descrip.style.display = 'none'
-    thkImg.style.display = 'flex'
-    thkImg.style.marginTop = '40px'
-    thkImg.style.marginBottom = '40px'
-    
-    // descrip.style.justifyContent = 'center'
-    // descrip.style.alignTimes = 'center'
-
-
 })
 
 btnSubmit.addEventListener('click', () => {
     if(enterNum == '') {
         alert('Пусто')
     } else {
-        
-        console.log('Вы выбрали - '  + enterNum)
+        iconStar.style.display = 'none'
+        title.style.display = 'none'
+        descrip.style.display = 'none'
+        thkImg.style.display = 'flex'
+        thkImg.style.marginTop = '40px'
+        thkImg.style.marginBottom = '40px'
+        btnRaiting.style.display = 'none'
+        result.style.display = 'flex'
+        result.style.alignItems = 'center'
+        result.style.justifyContent = 'center'
+        result.style.marginBottom = '20px'
+        btnSubmit.style.display = 'none'
+        bottom.style.display = 'block'
+
+        result.innerText = `You selected ${enterNum} of 5`
         enterNum = ''
         
     }
 
-    btnSubmit.style.background  = ''
-    descrip.style.visibility = 'visible'
+
 })
 
